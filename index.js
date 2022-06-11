eacb734cf4b70a4a1693e4f60cc823d1f81267af
jackline chepngeno <chepngenojackline0@gmail.com>
2022-06-11 17:02:13 +0300

    commit tree 793b27aaba3c848f0f7aa4a812385be314ce9561

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const totalBatteries = 0; //create variable
+
+const sumTotalBatteries = batteryBatches.reduce((prev, next) => prev + next, totalBatteries);
+
+console.log(sumTotalBatteries);
+
+// batteryBatches.reduce((currentBatteries, batteryBatch) => {
+//     const totalBatteries = batteryBatches.reduce((accum, batteryBatch)) => {
+    
+//         return currentBatteries + batteryBatch;
+//         return accum + batteryBatch;
+    
+//     }}, 0);
